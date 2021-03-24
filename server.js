fbb69f9e659e7e51adb702bb7d7fbec7fc2e2e09
Jeremiah Zagada <jeremiah.zagada16@gmail.com>
2021-03-24 18:53:32 -0400

    image endpoint (user 'entries') implemented

--- server.js
+++ server.js
@@ -98,17 +98,13 @@ app.get('/profile/:id', (req, res) => {
 
 app.put('/image', (req, res) => {
     const { id } = req.body;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        }
+    knex('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0]);
     })
-    if (!found) {
-        res.status(400).json('no such user exists');
-    }
+    .catch(err => res.status(400).json('Unable to get entries'))
 })
 
 app.listen(3000, () => {
