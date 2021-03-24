ef01489bb1eef6381c33ca32b619d36bd461d22a
Jeremiah Zagada <jeremiah.zagada16@gmail.com>
2021-03-24 18:42:40 -0400

    selection of user profiles implemented

--- server.js
+++ server.js
@@ -85,16 +85,15 @@ app.post('/register', (req, res) => {
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            return res.json(user);
-        }
+    knex.select('*').from('users').where({id})
+        .then(user => {
+            if (user.length) {
+                res.json(user[0]);
+            } else {
+                res.status(400).json('Not found');
+            }
     })
-    if (!found) {
-        res.status(400).json('no such user exists');
-    }
+    .catch(err => res.status(400).json('Error getting user'))
 })
 
 app.put('/image', (req, res) => {
