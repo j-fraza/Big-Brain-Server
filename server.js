06fdd9d00dd66a47dbc71ee5dc9d1916b61eb0db
Jeremiah Zagada <jeremiah.zagada16@gmail.com>
2021-04-01 18:13:36 -0400

    more controllers w/ dependency injections

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ const knex = require('knex')({
   });
 
 const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
 
 knex.select('*').from('users').then(data => {
     console.log(data);
@@ -23,54 +26,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('success');
-})
-
-app.post('/signin', (req, res) => {
-    knex.select('email', 'hash').from('login')
-        .where('email', '=', req.body.email)
-        .then(data => {
-            const isValid = bcrypt.compareSync(req.body.password, data[0].hash); 
-            if (isValid) {
-                return knex.select('*').from('users')
-                    .where('email', '=', req.body.email)
-                    .then(user => {
-                        res.json(user[0])
-                    })
-                    .catch(err => res.status(400).json('Unable to get user'))
-            } else {
-                res.status(400).json('Wrong Credentials')
-            }
-        })
-        .catch(err => res.status(400).json('Wrong Credentials'))
-})
-
-app.post('/register', (req, res) => { register.handleRegister(req, res, knex, bcrypt) })
-
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-    knex.select('*').from('users').where({id})
-        .then(user => {
-            if (user.length) {
-                res.json(user[0]);
-            } else {
-                res.status(400).json('Not found');
-            }
-    })
-    .catch(err => res.status(400).json('Error getting user'))
-})
-
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-    knex('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-        res.json(entries[0]);
-    })
-    .catch(err => res.status(400).json('Unable to get entries'))
-})
+app.get('/', (req, res) => { res.send('success') })
+app.post('/signin', signin.handleSignin(knex, bcrypt));
+app.post('/register', (req, res) => { register.handleRegister(req, res, knex, bcrypt) });
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, knex) });
+app.put('/image', (req, res) => { image.handleImage(req, res, knex) });
 
 app.listen(3000, () => {
     console.log('app is running on port 3000');
