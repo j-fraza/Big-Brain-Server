a617946376aecde6b8989f709aa54a925454db5e
Jeremiah Zagada <jeremiah.zagada16@gmail.com>
2021-04-01 18:01:16 -0400

    controllers + injection dependencies

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const knex = require('knex')({
     }
   });
 
+const register = require('./controllers/register');
+
 knex.select('*').from('users').then(data => {
     console.log(data);
 });
@@ -44,32 +46,7 @@ app.post('/signin', (req, res) => {
         .catch(err => res.status(400).json('Wrong Credentials'))
 })
 
-app.post('/register', (req, res) => {
-    const { email, name, password } = req.body;
-    const hash = bcrypt.hashSync(password);
-        knex.transaction(trx => {
-            trx.insert({
-                hash: hash,
-                email: email
-            })
-            .into('login')
-            .returning('email')
-            .then(loginEmail => {
-                return trx('users')
-                .returning('*')
-                .insert({
-                    email: loginEmail[0],
-                    name: name,
-                    joined: new Date()
-                }).then(user => {
-                    res.json(user[0]);
-                })
-            })
-            .then(trx.commit)
-            .catch(trx.rollback)
-        })
-        .catch(err => res.status(400).json('unable to register'))
-})
+app.post('/register', (req, res) => { register.handleRegister(req, res, knex, bcrypt) })
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
